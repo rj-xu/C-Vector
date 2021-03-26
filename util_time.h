5fa949193e582bb388bf86493adadf29bc34baa9
xuxiabb <xuxiabb@hotmail.com>
2021-03-26 20:26:08 +0800

    Create util_time.h

---
+++ util_time.h
@@ -0,0 +1,7 @@
+#include "util_define.h"
+
+extern int UtilSysGetTimeStamp(char *const timeStamp, const size_t maxSize, const char *const fmt);
+// extern int UtilSysGetTimeDiff();
+
+// extern int UtilSysGetHighPrecisionTime();
+// extern int UtilSysGetHighPrecisionTimeDiff();
