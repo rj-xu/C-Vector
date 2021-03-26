e2f07fcf662b1ccb39eb93b5476b18da9a1de306
xuxiabb <xuxiabb@hotmail.com>
2021-03-26 20:24:29 +0800

    Create util_interval.h

---
+++ util_interval.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "util_define.h"
+
+extern bool UtilItvlIntInLimit(const int value, const char *limit);
+extern bool UtilItvlDblInLimit(const double value, const char *limit);
