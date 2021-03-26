d78fa37ef902defab524c2bb942480ce399570cf
xuxiabb <xuxiabb@hotmail.com>
2021-03-26 20:23:01 +0800

    Create util_math.h

---
+++ util_math.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "util_define.h"
+
+#define UTIL_MATH_ESP (1e-6)
+
+// clang-format off
+extern bool UtilMathIntIntGt  (int a, int b);
+extern bool UtilMathIntIntGtEq(int a, int b);
+extern bool UtilMathIntIntEq  (int a, int b);
+extern bool UtilMathIntIntLs  (int a, int b);
+extern bool UtilMathIntIntLsEq(int a, int b);
+
+extern bool UtilMathDblDblGt  (double a, double b);
+extern bool UtilMathDblDblGtEq(double a, double b);
+extern bool UtilMathDblDblEq  (double a, double b);
+extern bool UtilMathDblDblLsEq(double a, double b);
+extern bool UtilMathDblDblLs  (double a, double b);
+
+
+extern bool UtilMathIntDblGt  (int a, double b);
+extern bool UtilMathIntDblGtEq(int a, double b);
+extern bool UtilMathIntDblEq  (int a, double b);
+extern bool UtilMathIntDblLsEq(int a, double b);
+extern bool UtilMathIntDblLs  (int a, double b);
+
+extern bool UtilMathDblIntGt  (double a, int b);
+extern bool UtilMathDblIntGtEq(double a, int b);
+extern bool UtilMathDblIntEq  (double a, int b);
+extern bool UtilMathDblIntLsEq(double a, int b);
+extern bool UtilMathDblIntLs  (double a, int b);
+// clang-format on
